9ad66010eb260e219991b683157b686293f2da7f
Dong-yeong0 <kevin@beomhoit.com>
2023-12-12 21:57:30 +0900

    Add simple kafka producer

--- python/producer/__main__.py
+++ python/producer/__main__.py
@@ -2,35 +2,37 @@ import json
 import os
 import random
 import time
-from kafka import KafkaProducer
-
+from kafka import KafkaProducer, errors
 
 def generate_message():
     while True:
         yield {
-            "topic": f"data_{random.randint(1, 10)}",
-            "data": 1
+            "data": random.randint(1, 99)
         }
         time.sleep(1)
 
+if __name__ == "__main__":
+    try:
+        producer = KafkaProducer(
+            bootstrap_servers=f"{os.environ.get('KAFKA_HOST')}:{os.environ.get('KAFKA_PORT')}",
+            compression_type="gzip",
+            api_version=(0,11,5),
+            key_serializer=lambda v: str(v).encode("utf-8"),
+            value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+            security_protocol="SASL_PLAINTEXT",
+            sasl_mechanism="PLAIN",
+            sasl_plain_username="admin",
+            sasl_plain_password="1234",
+            acks="all",
+        )
+        topic_generator = generate_message()
 
-
-if  __name__ == "__main__":
-    producer = KafkaProducer(
-        bootstrap_servers=f"{os.environ.get('KAFKA_HOST')}:{os.environ.get('KAFKA_PORT')}",
-        compression_type="gzip",
-        key_serializer=lambda v: str(v).encode("utf-8"),
-        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
-        security_protocol="SASL_PLAINTEXT",
-        sasl_plain_username="kafka",
-        sasl_plain_password="1234",
-        sasl_mechanism="PLAIN",
-        acks="all",
-    )
-    topic_generator = generate_message()
-
-    while True:
-        message = next(topic_generator)
-        producer.send(message["topic"], value=message["data"])
-        producer.flush()
-        print(f"Sent message to topic: {message['topic']}")
+        while True:
+            message = next(topic_generator)
+            producer.send(os.environ.get("KAFKA_TOPIC"), value=message)
+            producer.flush()
+            print(f"Sent message to topic: {message }")
+    except errors.KafkaError as ke:
+        print(f"KafkaError: {ke}")
+    finally:
+        producer.close()  # Always close the producer when done
